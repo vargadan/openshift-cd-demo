4071faaa7a788e8c06524beb5b1d5b79b9f9f370
vargadan <danielvrg@gmail.com>
2017-02-18 20:25:30 +0100

    adding roles properly

--- setup_oc_projects.sh
+++ setup_oc_projects.sh
@@ -1,8 +1,18 @@
-
 #!/bin/bash
 oc new-project dev --display-name="Tasks - Dev"
 oc new-project stage --display-name="Tasks - Stage"
 oc new-project cicd --display-name="CI/CD"
+
+oc logout
+
+oc login -u system:admin
+oc create -f https://raw.githubusercontent.com/jboss-openshift/application-templates/master/jboss-image-streams.json -n openshift
+oc logout
+
+oc login -u developer:developer
+
 oc policy add-role-to-user edit system:serviceaccount:cicd:default -n dev
 oc policy add-role-to-user edit system:serviceaccount:cicd:default -n stage
+oc policy add-role-to-user edit system:serviceaccount:cicd:jenkins -n dev
+oc policy add-role-to-user edit system:serviceaccount:cicd:jenkins -n stage
 oc process -f cicd-template.yaml | oc create -f -
